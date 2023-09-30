00dd5ca214206c91bd9c2691296f1b77ba51eae0
Mikhail <hudih@mail.ru>
2023-09-30 10:24:38 +0300

    Added time query to hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Println('Hello World!');
+  var hour:= ReadInteger('Enter current hour: ');
+  assert((hour >= 0) and (hour <= 23));
+  
+  if (hour >= 4) and (hour <= 10) then
+    Println('Good morning, world!');
+  else if (hour => 11) and (hour <= 16) then
+    Println('Good day, world!');
+  else if (hour >= 17) and (hour <= 22) then
+    Println('Good evening, world!')
+  else
+    Println('Goodnight, world');
 end.
